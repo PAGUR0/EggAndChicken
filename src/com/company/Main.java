569e55098b86101f47a279e34be4344f05b55b80
Paguro <ivankosaev2006@gmail.com>
2023-09-18 14:51:22 +0300

    1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,11 +16,15 @@ public class Main {
         try {
             chicken.join();
             egg.join();
+            if(chicken.isAlive()){
+                System.out.println("Спор окончен. Победило яйцо");
+            }
+            else{
+                System.out.println("Спор окончен. Победила курица");
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-
-        System.out.println("Спор окончен");
     }
 }
 
